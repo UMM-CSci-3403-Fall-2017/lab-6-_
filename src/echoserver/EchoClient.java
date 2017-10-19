f5e71d46645a0cb0acac65e16786b8e6cac0128c
nathan-beneke <benek020@morris.umn.edu>
2017-10-19 12:09:39 -0500

    Begin implementing code for EchoServer and create EchoClient.java Both based on https://gist.github.com/NicMcPhee/2060037163d0d7fb475b5e4395b9ec32

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -1,4 +1,28 @@
 package echoserver;
 
+import java.net.*;
+import java.io.*;
+
 public class EchoClient {
+
+    public static void main(String[] args){
+        try {
+            Socket socket = new Socket("127.0.0.1", 6013);
+            //change ip address to any ip or url
+
+            InputStream input = socket.getInputStream();
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+
+            String line;
+            while((line = reader.readLine()) != null){
+                System.out.println(line);
+            }
+
+            socket.close();
+        } catch (IOException ioe){
+            System.out.println("Shit got real");
+            System.err.println(ioe);
+        }
+    }
 }
--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -1,4 +1,48 @@
 package echoserver;
 
+import java.net.*;
+import java.io.*;
+
 public class EchoServer {
+
+    public static void main(String[] args){
+        try {
+            ServerSocket sock = new ServerSocket(6013);
+
+            while(true){
+                System.out.println("Got a request!");
+                Socket client = sock.accept();
+
+                //PrintWriter writer = new PrintWriter(client.getOutputStream(), true);
+                OutputStream out = client.getOutputStream();
+                BufferedReader reader = new BufferedReader(
+                        new InputStreamReader(client.getInputStream()));
+
+//                String line;
+//                while(true){
+//                    line = reader.readLine();
+//
+//                    if(line == null){
+//                        break;
+//                    }
+//
+//                    writer.println(line);
+//                    System.out.println(line);
+//                }
+
+                int nextByte;
+                while((nextByte = reader.read()) != -1){
+                    //writer.print((char) nextByte);
+                    out.write(nextByte);
+                    System.out.println((char) nextByte);
+                }
+                //writer.println();
+                System.out.println(nextByte);
+
+                client.close();
+            }
+        } catch (IOException ioe){
+            System.err.println(ioe);
+        }
+    }
 }
